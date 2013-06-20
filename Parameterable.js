a965ca0da86a20f6ecedc495e6aeb4f35d51a9a9
Aidan Wolter <awolter@kalkomey.com>
2013-06-20 11:54:20 -0500

    implemented module

--- Parameterable.js
+++ Parameterable.js
@@ -2,58 +2,65 @@
 // Aidan Wolter
 // Kalkomey Enterprises, Inc.
 
-function setParams(params) {
-    document.location.search = $.param(params);
-}
+(function() {
+    var urlParams = {
+        set: function(params) {
+            document.location.search = $.param(params);
+        },
 
-function getParams() {
-    var curParams = document.location.search.substr(1).split('&');
-    var hash = {};
+        list: function() {
+            var curParams = document.location.search.substr(1).split('&');
+            var hash = {};
 
-    for(var i = 0; i < curParams.length; i++) {
-        x = curParams[i].split('=');
+            for(var i = 0; i < curParams.length; i++) {
+                x = curParams[i].split('=');
 
-        //check if key is an array
-        var arrayRegex = /%5B\d%5D/;
-        if(x[0].match(arrayRegex)) {
+                //check if key is an array
+                var arrayRegex = /%5B\d%5D/;
+                if(x[0].match(arrayRegex)) {
 
-            //remove the bracket encoding from the key
-            x[0] = x[0].replace(arrayRegex, '');
+                    //remove the bracket encoding from the key
+                    x[0] = x[0].replace(arrayRegex, '');
 
-            //put the value into an array if it is the first object for the array
-            if(!hash[x[0]]) {
-                x[1] = [x[1]];
+                    //put the value into an array if it is the first object for the array
+                    if(!hash[x[0]]) {
+                        x[1] = [x[1]];
+                    }
+                }
+
+                //if key already exists in hash add value into array
+                if(hash[x[0]]) {
+                    hash[x[0]].push(x[1]);
+                }
+                else {
+                    hash[x[0]] = x[1];
+                }
+            }
+
+            return hash;
+        },
+
+        update: function(params) {
+            var curParams = this.list();
+
+            hash = $.extend(curParams,params);
+            this.set(hash);
+        },
+
+        remove: function(keys) {
+            if(keys instanceof Array) {
+                var curParams = this.list();
+
+                for(var i = 0;i < keys.length; i++) {
+                    delete curParams[keys[i]];
+                }
+                this.set(curParams);
+            }
+            else {
+                this.set(delete this.list()[key]);
             }
         }
+    };
 
-        //if key already exists in hash add value into array
-        if(hash[x[0]]) {
-            hash[x[0]].push(x[1]);
-        }
-        else {
-            hash[x[0]] = x[1];
-        }
-    }
-
-    return hash;
-}
-
-function updateParams(params) {
-    var curParams = getParams();
-
-    hash = $.extend(curParams,params);
-    setParams(hash);
-}
-
-function removeParams(keys) {
-    var curParams = getParams();
-
-    for(var i = 0;i < keys.length; i++) {
-        delete curParams[keys[i]];
-    }
-    setParams(curParams);
-}
-
-function removeParam(key) {
-    setParams(delete getParams()[key]);
-}
+    window.urlParams = urlParams;
+})();
